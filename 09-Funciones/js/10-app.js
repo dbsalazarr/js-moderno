5ece24f70cf7c54b7cebb881915712410b8c9a66
dbsalazarr <dbsalazarr@gmail.com>
2025-03-21 17:56:08 -0500

    Funciones, arrow functions, expresion functions

--- 09-Funciones/js/10-app.js
+++ 09-Funciones/js/10-app.js
@@ -6,16 +6,17 @@ const aprendiendo = function() {
     console.log('Aprendiendo JavaScript');
 }
 
-const aprendiendo = () =>  {
+const aprendiendo2 = () =>  {
     console.log('Aprendiendo JavaScript');
 }
 
 // una linea no requiere llaves
-const aprendiendo = () => console.log('Aprendiendo JavaScript');
+const aprendiendo3 = () => console.log('Aprendiendo JavaScript');
 
 // retornar un valor
-const aprendiendo = () => 'Aprendiendo JavaScript';
+const aprendiendo4 = () => 'Aprendiendo JavaScript';
 
 
-console.log(aprendiendo());
+aprendiendo()
+console.log(aprendiendo4())
 
