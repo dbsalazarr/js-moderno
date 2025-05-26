20eb2d06b4bc2a176d6a282c48fe3fef7893c0e6
dbsalazarr <dbsalazarr@gmail.com>
2025-05-26 08:45:04 -0500

    bucles en js while - do while

--- 11-Iteradores/js/04-app.js
+++ 11-Iteradores/js/04-app.js
@@ -1,15 +1,21 @@
 // Otro iterador muy común es el while loop, este se ejecuta mientras una condición sea verdadera..
 
 let i = 0;
-while (i < 10) { // condicion
+while (i <= 15) { // condicion    
 
-    // Bloque de código...
-    console.log(`Numero: ${i}`);
-    
-
-
+    if(i % 15 == 0){
+        console.log("Fizz and Buzz")
+    }else if(i % 5 == 0){
+        console.log("Buzz")
+    }else if(i % 3== 0){
+        console.log("Fizz")
+    }else{
+        console.log(i)
+    }
     i++; // incremento
 }
+
+
 
 // El while se ejecuta mientras una condición sea verdadera, por lo tanto si inicicializamos en 20, no hará nada..
 
--- 11-Iteradores/js/05-app.js
+++ 11-Iteradores/js/05-app.js
@@ -6,9 +6,17 @@
 let i = 0; // probar con 1000
 
 do {
-    console.log(`Numero: ${i}`)
-    i++;
-} while( i < 10 );
+    if(i % 15 == 0){
+        console.log("Fizz and Buzz")
+    }else if(i % 5 == 0){
+        console.log("Buzz")
+    }else if(i % 3== 0){
+        console.log("Fizz")
+    }else{
+        console.log(i)
+    }
+    i++
+} while( i <= 20 );
 
 
 // Ahora, lo que hace diferente a un while de un for o un do while, es que al menos se va a ejecutar una vez aunque la condición no se cumpla...
