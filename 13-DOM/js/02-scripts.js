bc70de756563e6f944fa72ec39f9f6b1120aa6dd
dbsrunsaac <dbsrunsaac@unsaac.edu.pe>
2025-06-20 14:36:10 -0500

    document y selectores en JS2

--- 13-DOM/js/02-scripts.js
+++ 13-DOM/js/02-scripts.js
@@ -2,20 +2,29 @@
 
 // Seleccionando el header...
 
-                // Todos tus selectores inician con document...
-const header = document.getElementsByClassName('header'); // Es muy importante las mayusculas y minusculas...
+// Todos tus selectores inician con document...
+const header = document.getElementsByClassName('contenedor'); // Es muy importante las mayusculas y minusculas...
 console.log(header);
 
-const hero = document.getElementsByClassName('contenido-hero');
-console.log(hero);
+for ( let i = 0; i < header.length; i++){
+    // console.log(header[i]);   
+}
+
+// const hero = document.getElementsByClassName('contenido-hero');
+// console.log(hero);
 
 
-// Como las classes se pueden repetir, obviamente todas las coincidencias de classes se asignaran a contenedores.
-const contenedores = document.getElementsByClassName('contenedor');
-console.log(contenedores);
+// // Como las classes se pueden repetir, obviamente todas las coincidencias de classes se asignaran a contenedores.
+// const contenedores = document.getElementsByClassName('contenedor');
+// console.log(contenedores);
 
 
-// Si una clase no existe, no va a retornar nada...
-const noExiste = document.getElementsByClassName('no-existe');
-console.log(noExiste);
+// // Si una clase no existe, no va a retornar nada...
+// const noExiste = document.getElementsByClassName('no-existe');
+// console.log(noExiste);
 
+let footer = document.getElementsByClassName("nav-footer")
+console.log(footer);
+
+let algo = document.getElementsByClassName("Nadar")
+console.log(algo);
